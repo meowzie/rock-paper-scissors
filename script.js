93d29c24677ba105f23ec5b623600d435f8aee20
meowzie <appenge2@gmail.com>
2021-12-09 19:14:03 +0200

    Created randomNum() function, which generates a random number from 0-2, and created computerPlay(theNumber) function, which returns Rock, Paper, or Scissors depending on the output of randomNum()

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("hi!")
+//create a function to generate a random number from 0-2//
+
+
+
+function randomNum() {
+    let theNum = Math.floor(Math.random()*3)
+    return theNum
+}
+
+//create a function that returns "rock", "paper", or "scissors" depending on randomNum()//
+function computerPlay(theNumber) {
+    theNumber = randomNum()
+    //create a conditional that returns rock, paper, or scissors for 0, 1, and 2 respectively//
+    let choice
+    if (theNumber===0) {
+        choice = "Rock"
+    } else if (theNumber===1) {
+        choice = "Paper"
+    } else {
+        choice = "Scissors"
+    }
+    return choice
+}
+
+console.log(computerPlay())
